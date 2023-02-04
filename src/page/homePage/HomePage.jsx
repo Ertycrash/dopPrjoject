63eb694d4a5ebd8bdfb18d37a474adc699227a5f
unknown <erkubekt@gmail.com>
2023-02-04 14:49:54 +0600

    master

--- src/page/homePage/HomePage.jsx
+++ src/page/homePage/HomePage.jsx
@@ -1,10 +1,8 @@
-import { CircularProgress } from '@mui/material'
 import React from 'react'
 
 function HomePage() {
   return (
     <div>
-     <CircularProgress size={100} color="success" />
     </div>
   )
 }
